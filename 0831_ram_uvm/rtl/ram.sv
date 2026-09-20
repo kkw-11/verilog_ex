760cb424f2a13a69497cc026657aa9a6ffb05590
kkw <kkw-11@naver.com>
2026-09-20 12:32:46 +0900

    feat:ram_uvm

---
+++ 0831_ram_uvm/rtl/ram.sv
@@ -0,0 +1,19 @@
+module ram(
+	input  logic       clk,
+	input  logic       we,
+	input  logic [7:0] addr,
+	input  logic [7:0] wdata,
+	output logic [7:0] rdata
+);
+	logic [7:0] ram_file[0:255];
+
+
+	always_ff @(posedge clk)begin
+		if(we)
+			ram_file[addr] <= wdata;
+	end
+
+	//CL output
+	assign rdata = ram_file[addr];
+
+endmodule
